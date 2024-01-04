ce1611bc87a5d46eff9addc24c6a43436a010336
Wes Matlock <wmatlock@gmail.com>
2024-01-04 05:54:25 -0700

    [WIP] Initial project creation

---
+++ TodoList/TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Wesley Matlock on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
