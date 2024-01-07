35d8a8db18ebeb748b9625de72ca763e50614dfe
Wes Matlock <wmatlock@gmail.com>
2024-01-07 11:21:26 -0700

    [WIP] ViewModel stuff

--- TodoList/TodoList/ViewModels/TaskViewModel.swift
+++ TodoList/TodoList/ViewModels/TaskViewModel.swift
@@ -17,4 +17,22 @@ final class TaskViewModel: ObservableObject {
         self.taskCreatedDate = task.createdDate
         self.taskPriority = task.isPriority
     }
+
+    func toggleStatus() {
+        isTaskCompleted.toggle()
+    }
+}
+
+final class TaskManager: ObservableObject {
+    @Published var finishedTasks = 0
+    @Published var totalTasks = 0
+    @Published var pendingTasks = 0
+    @Published var completionPercentage: Double = 0
+
+    @Published var categories: [Category] = [
+        Category(categoryName: "School", categoryColor: .init(hex: "189988"), associatedTasks: Array(repeating: Task(), count: 15), completedTask: 10),
+        Category(categoryName: "Personal", categoryColor: .blue, associatedTasks: Array(repeating: Task(), count: 10), completedTask: 3),
+        Category(categoryName: "Work", categoryColor: .red, associatedTasks: Array(repeating: Task(), count: 13), completedTask: 7)
+    ]
+
 }
